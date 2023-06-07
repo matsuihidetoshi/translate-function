13514271931fb368f6c9dc3c2c70b7916346ba3d
松井英俊 <dev.matsuihidetoshi@gmail.com>
2023-06-07 15:55:21 +0900

    [Add]TranslateFunctionStack

--- lib/translate-function-stack.ts
+++ lib/translate-function-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib'
+import { Stack, StackProps, aws_apigateway, Duration } from 'aws-cdk-lib'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class TranslateFunctionStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class TranslateFunctionStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const translateFunction = new NodejsFunction(scope, 'translate-function', {
+      runtime: Runtime.NODEJS_18_X,
+      functionName: 'translateFunction',
+      entry: 'src/translate-function.handler.ts',
+      timeout: Duration.seconds(60),
+      logRetention: 30,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TranslateFunctionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const restApi = new aws_apigateway.RestApi(this, 'translate-function-rest-api', {
+      restApiName: 'RestApiForTranslateFunction',
+      deployOptions: {
+        stageName: 'v1',
+      },
+    })
+
+    const restApiTranslateResource = restApi.root.addResource('translate')
+
+    restApiTranslateResource.addMethod('GET', new LambdaIntegration(translateFunction))
   }
 }
